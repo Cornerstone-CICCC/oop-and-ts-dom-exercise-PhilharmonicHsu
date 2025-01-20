e72c3c03776e651ed79543d5d2bf7e6926d9982a
Phil <xuyuwei19940909@gmail.com>
2025-01-20 12:09:19 -0800

    done

--- js/app.js
+++ js/app.js
@@ -1,3 +1,67 @@
 class Calculator {
+  constructor() {
+    this.num1 = document.querySelector('#num1');
+    this.num2 = document.querySelector('#num2');
+    this.result = document.querySelector('#result');
+    this.operation = document.querySelector('#operation');
 
+    document.querySelector('#calculateBtn')
+      .addEventListener('click', () => {
+
+        this.compute();
+      });
+  }
+
+  compute() {
+    if (this.verify()) {
+      switch (this.operation.value) {
+        case 'add': this.add(); break;
+        case 'subtract': this.subtract(); break;
+        case 'multiply': this.multiply(); break;
+        case 'divide': this.divide(); break;
+      }
+    }
+  }
+
+  verify() {
+    let errorMsg = [];
+    let isPass = true;
+
+    if (this.num1.value === '') {
+      errorMsg.push('the first number has not been filled in yet.');
+      isPass = false;
+    }
+
+    if (this.num2.value === '') {
+      errorMsg.push('the second number has not been filled in yet.')
+      isPass = false;
+    }
+
+    if (! isPass) {
+      alert(errorMsg.concat('\n'));
+    }
+
+    return isPass;
+  }
+
+  add() {
+    this.result.innerHTML = Number(this.num1.value) + Number(this.num2.value);
+  }
+
+  subtract() {
+    this.result.innerHTML = Number(this.num1.value) - Number(this.num2.value);
+  }
+
+  multiply() {
+    this.result.innerHTML = Number(this.num1.value) * Number(this.num2.value);
+  }
+
+  divide() {
+    const result = Number(this.num1.value) / Number(this.num2.value);
+    this.result.innerHTML = result.toFixed(2);
+  }
 }
+
+document.addEventListener('DOMContentLoaded', () => {
+  const calculator = new Calculator();
+})
